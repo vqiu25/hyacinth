6a7ad6fe9b1458112c393b5a991372de9cc2760f
victor <vqiu25@gmail.com>
2024-01-06 11:11:40 +1300

    Cosmetic: Created a Card View

--- hyacinth/Views/CardView.swift
+++ hyacinth/Views/CardView.swift
@@ -7,12 +7,59 @@
 
 import SwiftUI
 
-struct CardView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+internal struct CardView: View {
+    @State var task: TaskModel
+    
+    internal var body: some View {
+        
+        HStack(alignment: .center, spacing: 10) {
+            VStack(alignment: .leading, spacing: 5) {
+                // Task Name and Tag Name
+                Text(task.taskTitle)
+                    .font(.body)
+                    .fontWeight(.semibold)
+                Text(task.taskTag.tagTitle)
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal, 10)
+                    .foregroundColor(.white)
+                    .background(Color(task.taskTag.tagColour.colorValue))
+                    .cornerRadius(100)
+            }
+            
+            Spacer()
+            
+            // Task Completion Button
+            Button(action: {
+                task.taskIsCompleted.toggle()
+            }) {
+                Image(systemName: task.taskIsCompleted ? "checkmark.circle.fill" : "circle")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.lavendar)
+            }
+        }
+        .padding(.all, 10)
+        .background(Color.card)
+        // Context Menu
+        .contextMenu {
+            Button(action: {
+                // Edit Action
+            }) {
+                Text("Edit")
+                Image(systemName: "pencil")
+            }
+            Button(action: {
+                // Remove Action
+            }) {
+                Text("Remove")
+                Image(systemName: "trash")
+            }
+        }
     }
 }
 
 #Preview {
-    CardView()
+    CardView(task: TaskModel.sampleTask)
 }
