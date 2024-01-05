bbad7ce1690e59d400d81c77dc05c7a63fd51b3e
victor <vqiu25@gmail.com>
2024-01-06 10:24:54 +1300

    Structure: Created models for tasks

--- hyacinth/Models/TagModel.swift
+++ hyacinth/Models/TagModel.swift
@@ -5,4 +5,50 @@
 //  Created by Victor Esther Qiu on 5/01/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TagModel: Identifiable {
+    let id: UUID
+    private var title: String
+    private var colour: TagColour
+    
+    internal init(id: UUID = UUID(), title: String, colour: TagColour) {
+        self.id = id
+        self.title = title
+        self.colour = colour
+    }
+    
+    // Getters and Setters
+    internal var tagTitle: String {
+        get { self.title }
+        set { self.title = newValue }
+    }
+    
+    internal var tagColour: TagColour {
+        get { self.colour }
+        set { self.colour = newValue }
+    }
+}
+
+internal enum TagColour {
+    case crimson
+    case forest
+    case lavendar
+    case perwinkle
+    case custom(Color)
+    
+    internal var colorValue: Color {
+        switch self {
+        case .crimson:
+            return Color.crimson
+        case .forest:
+            return Color.forest
+        case .lavendar:
+            return Color.lavendar
+        case .perwinkle:
+            return Color.perwinkle
+        case .custom(let customColour):
+            return customColour
+        }
+    }
+}
--- hyacinth/Models/TaskModel.swift
+++ hyacinth/Models/TaskModel.swift
@@ -5,4 +5,41 @@
 //  Created by Victor Esther Qiu on 5/01/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TaskModel: Identifiable {
+    let id: UUID
+    private var title: String
+    private var date: Date
+    private var isCompleted: Bool
+    private var tag: TagModel
+    
+    internal init(id: UUID = UUID(), title: String, date: Date, isCompleted: Bool, tag: TagModel) {
+        self.id = id
+        self.title = title
+        self.date = date
+        self.isCompleted = isCompleted
+        self.tag = tag
+    }
+    
+    // Getters and Setters
+    internal var taskTitle: String {
+        get { self.title }
+        set { self.title = newValue }
+    }
+    
+    internal var taskDate: Date {
+        get { self.date }
+        set { self.date = newValue }
+    }
+    
+    internal var taskIsCompleted: Bool {
+        get { self.isCompleted }
+        set { self.isCompleted = newValue }
+    }
+    
+    internal var taskTag: TagModel {
+        get { self.tag }
+        set { self.tag = newValue }
+    }
+}
