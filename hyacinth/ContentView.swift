9b8af199b8d7a06eb05077a980438c17c6df3442
victor <vqiu25@gmail.com>
2024-01-05 23:21:42 +1300

    Initial Commit

---
+++ hyacinth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hyacinth
+//
+//  Created by Victor Esther Qiu on 5/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hyacinth/hyacinthApp.swift
@@ -0,0 +1,17 @@
+//
+//  hyacinthApp.swift
+//  hyacinth
+//
+//  Created by Victor Esther Qiu on 5/01/24.
+//
+
+import SwiftUI
+
+@main
+struct hyacinthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
